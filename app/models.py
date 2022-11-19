4674908500f219ce09421f273cce9ede7675f52b
0ceanlight <imahacubak@gmail.com>
2022-11-19 01:52:55 +0100

    Add databases to models.py

--- app/models.py
+++ app/models.py
@@ -3,3 +3,19 @@ from django.db import models
 # Create your models here.
 
 # admin.site.register(Table Name)
+class User(models.Model):
+    # note: charfield has max length 255 chars
+    # TextField allows for longer entries
+    # null/blank: we can't submit a form with no values
+    handle = models.CharField(max_length=28)
+    password = models.CharField(max_length=255)
+
+class Uploads(models.Model):
+    title = models.CharField(max_length=64)
+    content = models.TextField()
+    # auto_now can change
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class Links(models.Model):
+    url = models.TextField()
