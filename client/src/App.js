a6aa8731a35c2582332e41219ed8a4de97e5d74e
Jay <jay0816@outlook.com>
2022-11-19 21:51:37 +0100

    fix burgs

--- client/src/App.js
+++ client/src/App.js
@@ -8,16 +8,20 @@ import { useState } from 'react';
 
 function App() {
   const [ buttonPopup, setButtonPopup ] = useState(false);
+  const [ title, setTitle ] = useState('');
+  const [ content, setContent ] = useState('');
+  const [ longitude, setLongitude ] = useState('');
+  const [ latitude, setLatitude ] = useState('');
   
   return (
     <div class='list_page_wrapper'>
-      <Popup trigger={ buttonPopup } setTrigger={ setButtonPopup } />
+      <Popup trigger={ buttonPopup } setTrigger={ setButtonPopup } title={title} content={content}/>
       <div class="list_flexbox" style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
         <div style={{width: '20%'}}></div>
         {/* TODO: fix backgroundColor, style w/ transparency etc. */}
         <div>
           <div style={{padding: '15px'}}>
-            <Loctab />
+            <Loctab setLongitude={setLongitude} setLatitude={setLatitude}/>
           </div>
 
           <div style={{padding: '15px', position: 'sticky', top: '0'}}>
@@ -25,7 +29,7 @@ function App() {
           </div>
 
           <div class='list_view_wrapper'>
-            <ListView setTrigger={setButtonPopup}/>
+            <ListView setTrigger={setButtonPopup} setTitle={setTitle} setContent={setContent} longitude={longitude} latitude={latitude}/>
           </div>
           
         </div>
