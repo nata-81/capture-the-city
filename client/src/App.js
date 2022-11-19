cc3f6c80ae4c0bee6059c0fc21c8788dda508e67
0ceanlight <imahacubak@gmail.com>
2022-11-19 12:07:13 +0100

    Implement prototype list view in client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import ListView from './listPage/List';
 
 function App() {
   return (
-      'Hello World'
+    <div>
+      <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+        <div style={{width: '20%'}}></div>
+        {/* TODO: fix backgroundColor, style w/ transparency etc. */}
+        <div style={{borderRadius: '20px', border: '2px solid Black', backgroundColor: '#B0B0FF', padding: '15px'}}>
+          <ListView/>
+        </div>
+        <div style={{width: '20%'}}></div>
+      </div>
+    </div>
   );
 }
 
