f16d18450b2c10cd33087d595ebc63eba58a1724
0ceanlight <imahacubak@gmail.com>
2022-11-20 00:55:54 +0100

    Add valid request to form

--- client/src/listPage/Form.js
+++ client/src/listPage/Form.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 
-const Form = () => {
+const Form = (props) => {
 
+    const [title, setTitle] = useState('')
+    const [cont, setCont] = useState('')
     const [lat, setLat] = useState('');
     const [long, setLong] = useState('');
 
@@ -10,38 +12,50 @@ const Form = () => {
     }, [])
 
     let postUploads = async () => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            let lat = position.coords.latitude;
-            let long = position.coords.longitude;
-        
-            setLat(lat.toFixed(7));
-            setLong(long.toFixed(7));
-        });
+      navigator.geolocation.getCurrentPosition((position) => {
+          let lat = position.coords.latitude;
+          let long = position.coords.longitude;
+      
+          setLat(lat.toFixed(16));
+          setLong(long.toFixed(16));
+      });
     
-        const rawResponse = await fetch('https://127.0.0.1/upload/create/', {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({title: 'tmp', body: 'Textual content', longitude: long, latitude: lat})
-        });
-        const content = await rawResponse.json();
-        console.log(content);
-      };
+      const rawResponse = await fetch('https://127.0.0.1/upload/create/', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({title: title, body: cont, longitude: long, latitude: lat})
+      });
+      const content = await rawResponse.json();
+    };
+
+    function updateTitle(evt) {
+      const val = evt.target.value;
+      setTitle(val);
+      console.log(title)
+    }
+
+    function updateCont(evt) {
+      const val = evt.target.value;
+      setCont(val);
+    }
     
     return (
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <input type="text" placeholder="What's Happening?" style={{ backgroundColor: 'transparent', borderTop: 'None', borderRight: 'None', borderLeft: 'None', fontSize: '30px' }}></input>
-            <div style={{ height: '20px' }}></div>
-            <input type="textarea" placeholder="Describe more ..." style={{ backgroundColor: 'transparent', borderTop: 'None', borderRight: 'None', borderLeft: 'None', fontSize: '20px' }}></input>
-            <div style={{ height: '20px' }}></div>
-            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-              <button class='submit_button'>Submit</button>
-              <input type="text" placeholder="Send as Anonymous" style={{ backgroundColor: 'transparent', borderTop: 'None', borderRight: 'None', borderLeft: 'None', fontSize: '14px' }}></input>
-            </div>
-            <div style={{ height: '20px' }}></div>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+
+        <input type="text" onChange={evt => updateTitle(evt)} placeholder="What's Happening?" style={{ backgroundColor: 'transparent', borderTop: 'None', borderRight: 'None', borderLeft: 'None', fontSize: '30px' }}></input>
+        <div style={{ height: '20px' }}></div>
+        
+        <input type="textarea" onChange={evt => updateCont(evt)} placeholder="Describe more ..." style={{ backgroundColor: 'transparent', borderTop: 'None', borderRight: 'None', borderLeft: 'None', fontSize: '20px' }}></input>
+        <div style={{ height: '20px' }}></div>
+        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+          <button class='submit_button' onClick={() => postUploads}>Submit</button>
+          <input type="text" placeholder="Send as Anonymous" style={{ backgroundColor: 'transparent', borderTop: 'None', borderRight: 'None', borderLeft: 'None', fontSize: '14px' }}></input>
         </div>
+        <div style={{ height: '20px' }}></div>
+      </div>
     );
 }
 
