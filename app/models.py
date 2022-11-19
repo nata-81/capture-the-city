f77279757ce15d42f6b9cf1bde386f76963affd2
Jay <jay0816@outlook.com>
2022-11-19 01:27:44 +0100

    url setup

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+
+# admin.site.register(Table Name)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/helloworld/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of notes'
+        },
+    ]
+    return JsonResponse(routes, safe=False)
